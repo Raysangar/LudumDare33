9226c75b2ad04b47b917fc5db855d7ee48d3058c
Bryan Garzon <gabra666@hotmail.com>
2015-08-22 20:53:37 +0100

    Attack SeordController

---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordController : MonoBehaviour {
+
+	public float swordDamage = 10;
+	public Collider swordTrigger;
+
+	public void EnableSwordTrigger(){
+		swordTrigger.enabled = true;
+	}
+
+	public void DisableSwordTrigger(){
+		swordTrigger.enabled = false;
+	}
+
+	public void ApplySwordDamage(Collider other){
+		other.GetComponent<HealthSystem> ().ReceiveDamage(swordDamage);
+	}
+
+}
