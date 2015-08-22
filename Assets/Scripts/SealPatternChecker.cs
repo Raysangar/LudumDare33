6dd617bc629ac96f3792c01f8e29b5e7ed437f1e
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2015-08-22 18:10:07 +0100

    Seals implemented

---
+++ Assets/Scripts/SealPatternChecker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SealPatternChecker : MonoBehaviour {
+    [SerializeField]
+    private Transform startingPoint, finishPoint;
+
+    [SerializeField]
+    private float thresHold = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool checkSealCompleted(Vector3 mousePoint)
+    {
+        return Vector3.Distance(mousePoint, finishPoint.position) <= thresHold;
+    }
+
+    public bool sealStarted(Vector3 mousePoint)
+    {
+        return Vector3.Distance(mousePoint, startingPoint.position) <= thresHold;
+    }
+}
