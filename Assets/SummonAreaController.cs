de9dbce7c6750a7f03138a4cc5c648f099b63602
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2015-08-23 16:45:22 +0100

    Summon mechanic  finished

---
+++ Assets/SummonAreaController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SummonAreaController : MonoBehaviour {
+    private bool playerOnSummonArea;
+    private BloodCollector bloodCollector;
+    private Summoner summoner;
+
+    [SerializeField]
+    private Text textHint;
+
+    [SerializeField]
+    private GameObject hint;
+
+	// Use this for initialization
+	void Start () {
+        playerOnSummonArea = false;
+        hint.SetActive(false);
+        bloodCollector = GameObject.Find("Player").GetComponent<BloodCollector>();
+        summoner = GameObject.Find("Altar").GetComponent<Summoner>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerOnSummonArea && !summoner.playerIsADemon)
+        {
+            textHint.text = (summoner.getNecessaryBlood() <= bloodCollector.accumulatedBlood) ?
+                "Press Space to Summon the Demon" : "Collect Blood for Summoning the Demon";
+            if (bloodCollector.accumulatedBlood >= summoner.getNecessaryBlood() && Input.GetKeyDown(KeyCode.Space))
+                summoner.summonDemon();
+            hint.SetActive(true);
+        }
+        else
+            hint.SetActive(false);
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.layer == 10)
+            playerOnSummonArea = true;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.layer == 10)
+            playerOnSummonArea = false;
+    }
+}
