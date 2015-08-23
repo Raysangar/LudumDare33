a48778f535ce33de6c709c24a93a37ad641cd679
Bryan Garzon <gabra666@hotmail.com>
2015-08-23 12:09:18 +0100

    BloodSystem NavController y MenuController CutisModel con animaciones rotas :(

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	NavigationController navController;
+	// Use this for initialization
+	void Start () {
+		navController = NavigationController.Instance;
+	}
+
+	public void GoToMenuScene(){
+		navController.GoToScene (navController.menuScene);
+	}
+	
+	public void GoToHighScoreScene(){
+		navController.GoToScene (navController.highScoreScene);
+	}
+	
+	public void GoToGameScene(){
+		navController.GoToScene (navController.gameScene);
+	}
+}
